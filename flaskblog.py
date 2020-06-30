54118ff56a8715caf1eeedfec3aadc3134adc5da
Rohit <r4rohit002@gmail.com>
2020-06-29 21:23:14 -0700

    Added render template

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/home')
 @app.route('/')
 def hello_world():
-    return "Hi there how you doing? I am doing great"
+    return render_template('home.html')
 
 @app.route('/about')
 def about_author():
-    return "Hi there how you doing? I am doing great"    
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=4455,debug=True) 
