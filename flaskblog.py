9745f694c4156f936dd5c76e49626243caaef82e
Rohit <r4rohit002@gmail.com>
2020-06-29 23:31:42 -0700

    Added list to render dynamically

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+            {
+                'author': 'Rohit',
+                'tittle': 'First Blog post',
+                'content': 'First blog post content. This is the dummy content',
+                'date_posted': '29 June 2020'
+
+            },
+            {
+                'author': 'Jon Doe',
+                'tittle': 'Second Blog post',
+                'content': 'Second blog post content. This is the second content',
+                'date_posted': '2 June 2020'
+
+            }
+]
+
 @app.route('/home')
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', posts = posts)
 
 @app.route('/about')
 def about_author():
