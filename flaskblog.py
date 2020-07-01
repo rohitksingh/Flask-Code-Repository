9373d7436feb9aa5f8e83020200da6ccb9133e30
Rohit <r4rohit002@gmail.com>
2020-07-01 15:22:26 -0700

    Added validation for login page

--- flaskblog.py
+++ flaskblog.py
@@ -50,9 +50,15 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'user@gmail.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful', 'error')    
     return render_template('login.html', form=form)        
 
 if __name__ == '__main__':
